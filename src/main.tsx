f8bd65261e06ede3fa96cbc0500e07b5b6acedfc
advitiya sharma <a687shar@uwaterloo.ca>
2025-07-18 17:16:11 -0400

    Fix project structure and deployment issues - Move CoverPage.tsx to src directory and update imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CoverPage from '../CoverPage.tsx'
+import CoverPage from './CoverPage.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
